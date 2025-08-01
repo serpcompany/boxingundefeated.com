1fb1bf8727da7ab203e93fc18ffb6a327f179733
devinschumacher <devin@serp.co>
2025-08-01 10:10:51 -0700

    [hotkey]

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 // Logo component with boxing glove icon
-const appConfig = useAppConfig()
+// const appConfig = useAppConfig()
 </script>
 
 <template>
   <NuxtLink to="/" class="flex items-center space-x-2 text-lg font-bold">
-    </div>
-    <span class="text-zinc-900">{{ appConfig.site.name }}</span>
+    <!-- <span class="text-zinc-900">{{ appConfig.site.name }}</span> -->
     <span class="text-zinc-900">
-      <img src="/logo.svg" alt="Logo" class="h-8 w-8" />
+      <img src="/logo.svg" alt="boxing undefeateed logo" class="h-8 w-8" />
     </span>
   </NuxtLink>
 </template>
+    <!-- <span class="text-zinc-900">{{ appConfig.site.name }}</span> -->
